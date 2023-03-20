2b1b3f4ba4380bc12a97ce6c2c7c339dee143d67
Fetsundscripter <jens.petter.haraldsen@gmail.com>
2023-03-20 22:46:45 +0100

    getting the bars up but not passing anymore testes

--- index.js
+++ index.js
@@ -15,27 +15,53 @@ function App() {
     return (
         <div>
             <h1>Hello world</h1>
-            </div>
-    );
+            <BarChart 
+            data={countryData} 
+            height={500} 
+            widthOfBar={5} 
+            width={countryData.lenght * 5} 
+            dataType={"casesPerOneMillion"}
+             />
+         </div>
+        );
+    }
+
     function BarChart({data, height, width, widthOfBar, dataType}) {
         React.useEffect(() =>{
             createBarChart();
-        }, [])
+        }, [data]);
 
         const createBarChart = () => {
-            const countryData = data.map(country => country['casesPerOneMillion'])
+            const countryData = data.map(country => country['casesPerOneMillion']);
+            console.log("countryData", countryData);
             const countries = data.map(country => country.country);
+
+            let tooltip = d3.select(".visHolder")
+            .append("div")
+            .attr("id", "tooltip")
+            .style("opacity", 0);
 
             const dataMax = d3.max(countryData);
             const yScale = d3.scaleLinear().domain([0, dataMax]).range([0, height])
             d3.select("svg").selectAll("rect").data(countryData).enter().append("rect");
+            d3.select("svg")
+            .selectAll("rect")
+            .data(countryData)
+            .style("fill", (d, i) => (i % 2 == 0 ? "#9595ff" : "#44ff44"))
+            .attr("x", (d, i) => i * widthOfBar)
+            .attr("y", (d) => height - yScale(d + dataMax * 0.1))
+            .attr("height", (d, i) => yScale(d + dataMax * 0.1))
+            .attr("width", widthOfBar)
+            .on("mouseover", (d, i) => {
+                tooltip.html(countries[i] + '<br/> ${dataType}')
+            });
         };
 
-        return {
+        return (
             <>
-            <svg width=(width) height=(height)></svg>
+            <svg width={width} height={height}></svg>
             </>
-        };
+        );
 }
 
 ReactDOM.render(<App/>, document.getElementById('root'));
