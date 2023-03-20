2122d0ba2bf3fd7cdf86220ec4bb34f2148d74f2
Fetsundscripter <jens.petter.haraldsen@gmail.com>
2023-03-20 23:03:22 +0100

    getting better

--- index.js
+++ index.js
@@ -15,13 +15,15 @@ function App() {
     return (
         <div>
             <h1>Hello world</h1>
+            <div className="visHolder">
             <BarChart 
             data={countryData} 
             height={500} 
             widthOfBar={5} 
             width={countryData.lenght * 5} 
             dataType={"casesPerOneMillion"}
-             />
+            />
+            </div>
          </div>
         );
     }
@@ -36,7 +38,8 @@ function App() {
             console.log("countryData", countryData);
             const countries = data.map(country => country.country);
 
-            let tooltip = d3.select(".visHolder")
+            let tooltip = d3
+            .select(".visHolder")
             .append("div")
             .attr("id", "tooltip")
             .style("opacity", 0);
@@ -53,7 +56,11 @@ function App() {
             .attr("height", (d, i) => yScale(d + dataMax * 0.1))
             .attr("width", widthOfBar)
             .on("mouseover", (d, i) => {
-                tooltip.html(countries[i] + '<br/> ${dataType}')
+                tooltip.style("opacity", 0.9);
+                tooltip
+                .html(countries[i] + `<br/> ${dataType}: ` + d)
+                .style("left", i * widthOfBar + 20 + "px")
+                .style*("top", d3.event.pageY - 170 + "px");
             });
         };
 
