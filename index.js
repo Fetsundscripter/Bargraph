494c105c8173316cd4ae2309e58fb90988dbc5d1
Fetsundscripter <jens.petter.haraldsen@gmail.com>
2023-03-20 22:07:31 +0100

    compliting the d3. selection

--- index.js
+++ index.js
@@ -17,6 +17,25 @@ function App() {
             <h1>Hello world</h1>
             </div>
     );
+    function BarChart({data, height, width, widthOfBar, dataType}) {
+        React.useEffect(() =>{
+            createBarChart();
+        }, [])
+
+        const createBarChart = () => {
+            const countryData = data.map(country => country['casesPerOneMillion'])
+            const countries = data.map(country => country.country);
+
+            const dataMax = d3.max(countryData);
+            const yScale = d3.scaleLinear().domain([0, dataMax]).range([0, height])
+            d3.select("svg").selectAll("rect").data(countryData).enter().append("rect");
+        };
+
+        return {
+            <>
+            <svg width=(width) height=(height)></svg>
+            </>
+        };
 }
 
 ReactDOM.render(<App/>, document.getElementById('root'));
