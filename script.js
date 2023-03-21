79eba19f04c8ace3d42576e394b83c649771e97c
Fetsundscripter <jens.petter.haraldsen@gmail.com>
2023-03-21 20:58:30 +0100

    passing more tests this round. doing something right

--- script.js
+++ script.js
@@ -29,29 +29,56 @@ let generateScales = () => {
                     .range([0, height - (2*padding)])
 
     xScale = d3.scaleLinear()
-                    .domain([0, value.length -1])
+                    .domain([0, values.length -1])
                     .range([padding, width - padding])
 
-    let datesArray = value.map((item) => {
+    let datesArray = values.map((item) => {
         return new Date(item[0])
     })
 
     xAxisScale = d3.scaleTime()
-                    .domain([d3.min(datesArray, d3.max(datesArray)])
-                    .range([padding, width - padding])
+                    .domain([d3.min(datesArray), d3.max(datesArray)])
+                    .range([padding, width-padding])
 
     yAxisScale = d3.scaleLinear()
-                   .domain([0, d3.max(values, (tiem) => {
-                    
+                   .domain([0, d3.max(values, (item) => {
+                        return item[1]
                    })])
+                   .range([height - padding, padding])
 }
 
 let drawBars = () => {
-
+    svg.selectAll('rect')
+        .data(values)
+        .enter()
+        .append('rect')
+        .attr('class', 'bar')
+        .attr('width', (width - (2 * padding)) / values.length)
+        .attr('data-date', (item) =>{
+            return item[0]
+        })
+        .attr('data-gdp', (item) =>{
+            return item[1]
+        })
+        .attr('height', (item) => {
+            return heightScale(item[1])
+        })
 }
 
 let generateAxes = () => {
 
+    let xAxis = d3.axisBottom(xAxisScale)
+    let yAxis = d3.axisLeft(yAxisScale)
+
+    svg.append('g')
+        .call(xAxis)
+        .attr('id', 'x-axis')
+        .attr('transform', 'translate(0, ' + (height-padding) + ')')
+
+    svg.append('g')
+        .call(yAxis)
+        .attr('id', 'y-axis')
+        .attr('transform', 'translate(' + padding + ', 0)')
 }
 
 req.open('GET', url, true)
